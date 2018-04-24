bd228cc44dec1d24c582d893baa07158c9210d46
akshay <akshaychopra96@gmail.com>
2018-04-24 12:25:15 +0530

    cleaning up folder structure

---
+++ Downloads/Workshop Attendance/app/src/main/java/com/example/akshay/Workshop Attendance/Login_Fragment.java
@@ -0,0 +1,244 @@
+package com.example.akshay.WorkshopAttendance;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.akshay.WorkshopAttendance.Data.DatabaseHelper;
+import com.example.akshay.WorkshopAttendance.Data.SignUpContract;
+
+public class Login_Fragment extends Fragment implements OnClickListener {
+	private static View view;
+
+	private static EditText emailid, password;
+	private static Button loginButton;
+	private static TextView forgotPassword, signUp;
+	private static CheckBox show_hide_password;
+	private static LinearLayout loginLayout;
+	private static Animation shakeAnimation;
+	private static FragmentManager fragmentManager;
+	DatabaseHelper helper ;
+	SQLiteDatabase mDb ;
+	SharedPreferences sp;
+
+	boolean login = false;
+
+	public Login_Fragment() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view = inflater.inflate(R.layout.login_layout, container, false);
+		initViews();
+		setListeners();
+		helper = new DatabaseHelper(getActivity());
+		mDb = helper.getWritableDatabase();
+		return view;
+	}
+
+	// Initiate Views
+	private void initViews() {
+		fragmentManager = getActivity().getSupportFragmentManager();
+
+		emailid = (EditText) view.findViewById(R.id.login_emailid);
+		password = (EditText) view.findViewById(R.id.login_password);
+		loginButton = (Button) view.findViewById(R.id.loginBtn);
+		forgotPassword = (TextView) view.findViewById(R.id.forgot_password);
+		signUp = (TextView) view.findViewById(R.id.createAccount);
+		show_hide_password = (CheckBox) view
+				.findViewById(R.id.show_hide_password);
+		loginLayout = (LinearLayout) view.findViewById(R.id.login_layout);
+
+		// Load ShakeAnimation
+		shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+				R.anim.shake);
+
+		// Setting text selector over textviews
+		@SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+		try {
+			ColorStateList csl = ColorStateList.createFromXml(getResources(),
+					xrp);
+
+			forgotPassword.setTextColor(csl);
+			show_hide_password.setTextColor(csl);
+			signUp.setTextColor(csl);
+		} catch (Exception e) {
+		}
+	}
+
+	// Set Listeners
+	private void setListeners() {
+		loginButton.setOnClickListener(this);
+		forgotPassword.setOnClickListener(this);
+		signUp.setOnClickListener(this);
+
+		// Set check listener over checkbox for showing and hiding password
+		show_hide_password
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+					@Override
+					public void onCheckedChanged(CompoundButton button,
+							boolean isChecked) {
+
+						// If it is checkec then show password else hide
+						// password
+						if (isChecked) {
+
+							show_hide_password.setText(R.string.hide_pwd);// change
+																			// checkbox
+																			// text
+
+							password.setInputType(InputType.TYPE_CLASS_TEXT);
+							password.setTransformationMethod(HideReturnsTransformationMethod
+									.getInstance());// show password
+						} else {
+							show_hide_password.setText(R.string.show_pwd);// change
+																			// checkbox
+																			// text
+
+							password.setInputType(InputType.TYPE_CLASS_TEXT
+									| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+							password.setTransformationMethod(PasswordTransformationMethod
+									.getInstance());// hide password
+
+						}
+
+					}
+				});
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.loginBtn:
+			boolean check = checkValidation();
+			if(check){
+				sp = getActivity().getSharedPreferences("login",Context.MODE_PRIVATE);
+				sp.edit().putBoolean("isLogin",false).commit();
+				Intent dashboardIntent = new Intent(getContext(),DashboardActivity.class);
+				startActivity(dashboardIntent);
+
+			}
+			break;
+
+		case R.id.forgot_password:
+
+			// Replace forgot password fragment with animation
+			fragmentManager
+					.beginTransaction()
+					.setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+					.replace(R.id.frameContainer,
+							new ForgotPassword_Fragment(),
+							Utils.ForgotPassword_Fragment).commit();
+			break;
+		case R.id.createAccount:
+
+			// Replace signup frgament with animation
+			fragmentManager
+					.beginTransaction()
+					.setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+					.replace(R.id.frameContainer, new SignUp_Fragment(),
+							Utils.SignUp_Fragment).commit();
+			break;
+		}
+
+	}
+
+	// Check Validation before login
+	private boolean checkValidation() {
+		// Get email id and password
+		String getEmailId = emailid.getText().toString();
+		String getPassword = password.getText().toString();
+
+		// Check patter for email id
+		Pattern p = Pattern.compile(Utils.regEx);
+
+		Matcher m = p.matcher(getEmailId);
+
+		// Check for both field is empty or not
+		if (getEmailId.equals("") || getEmailId.length() == 0
+				|| getPassword.equals("") || getPassword.length() == 0) {
+			loginLayout.startAnimation(shakeAnimation);
+			new CustomToast().Show_Toast(getActivity(), view,
+					"Enter both credentials.");
+			return false;
+
+		}
+		// Check if email id is valid or not
+		else if (!m.find()) {
+			new CustomToast().Show_Toast(getActivity(), view,
+					"Your Email Id is Invalid.");
+		return false;
+		}
+
+			Cursor mCursor = mDb.query(
+					SignUpContract.SignUpEntry.TABLE_NAME,
+					null,
+					null,
+					null,
+					null,
+					null,
+					null);
+
+		Log.d("tag","-->"+mCursor.moveToNext());
+
+		if(mCursor.moveToNext()){
+			while (!(getEmailId.toLowerCase().equalsIgnoreCase(mCursor.getString(mCursor.getColumnIndex(SignUpContract.SignUpEntry.COLUMN_EMAIL))))){
+				mCursor.moveToNext();
+				if(mCursor.isAfterLast()){
+					break;
+				}
+				}
+
+			if(mCursor.isAfterLast()){
+				new CustomToast().Show_Toast(getActivity(),view,"Please SignUp with a new email id");
+			return false;
+			}
+
+			else if(mCursor.getString(mCursor.getColumnIndex(SignUpContract.SignUpEntry.COLUMN_PASSWORD)).equals(getPassword)){
+				return true;
+
+			}
+
+			else
+			{
+				new CustomToast().Show_Toast(getActivity(),view,"Password doesn't match");
+				return false;
+			}
+
+
+		}
+		new CustomToast().Show_Toast(getActivity(),view,"Please create a new Account");
+		return false;
+	}
+}
